d14b5701b1bcafa139a317b810bba733d3cb802e
jeremiaheb <jblondeau@gmail.com>
2015-04-06 10:30:09 -0400

    convert coffee to javascript

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,5 @@
-jQuery ->
-  $("a[rel=popover]").popover()
-  $(".tooltip").tooltip()
-  $("a[rel=tooltip]").tooltip()
+jQuery(function() {
+  $("a[rel=popover]").popover();
+  $(".tooltip").tooltip();
+  return $("a[rel=tooltip]").tooltip();
+});
