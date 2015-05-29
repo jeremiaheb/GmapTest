55baf160a66a1b8e661562b220ec967233cec8cd
jeremiaheb <jblondeau@gmail.com>
2015-05-29 10:18:45 -0400

    added seed data for STX and SEFCRI

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,15 @@
   #end
 #end
 
-#Location.delete_all("region_id = 3")
-#open("db/SeedData/SEFCRI2014.csv") do |samples|
-  #samples.read.each_line do |sample|
-    #id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
-    #Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
-  #end
-#end
+Location.delete_all("region_id = 3")
+open("db/SeedData/SEFCRI_2015.csv") do |samples|
+  samples.read.each_line do |sample|
+    id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
+    Location.create(:site => id, :latitude => lat, :longitude => lon, :region_id => reg, :level => level, :need => need, :color => color, :is_reserved => reserve, :depth => depth)
+  end
+end
 
-Location.delete_all
+Location.delete_all("region_id = 5")
 open("db/SeedData/STX_2015.csv") do |samples|
   samples.read.each_line do |sample|
     id, lat, lon, reg, level, need, color, reserve, depth = sample.chomp.split(",")
